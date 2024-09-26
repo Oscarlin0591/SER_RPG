6656f5c92295d7b90b1e5238597f177f7ffc0193
Oscar Lin <Oscar.Lin@quinnipiac.edu>
2024-09-25 22:29:00 -0400

    Added music files, fixed the map transition to not endlessly initiate the map. No progress on the music area.

---
+++ src/NPCs/Island.java
@@ -0,0 +1,44 @@
+package NPCs;
+
+import Builders.FrameBuilder;
+import Engine.GraphicsHandler;
+import Engine.ImageLoader;
+import GameObject.Frame;
+import GameObject.ImageEffect;
+import GameObject.SpriteSheet;
+import Level.NPC;
+import Utils.Point;
+
+import java.util.HashMap;
+
+// This class is for the walrus NPC
+public class Island extends NPC {
+
+    public Island(int id, Point location) {
+        super(id, location.x, location.y, new SpriteSheet(ImageLoader.load("Island.png"), 16, 16), "STAND_LEFT");
+    }
+
+    @Override
+    public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
+        return new HashMap<String, Frame[]>() {{
+            put("STAND_LEFT", new Frame[] {
+                    new FrameBuilder(spriteSheet.getSprite(0, 0))
+                            .withScale(6)
+                            .withBounds(8, 8, 8, 8)
+                            .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
+                            .build()
+            });
+            put("STAND_RIGHT", new Frame[] {
+                   new FrameBuilder(spriteSheet.getSprite(0, 0))
+                           .withScale(6)
+                           .withBounds(8, 8, 16, 16)
+                           .build()
+           });
+        }};
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+}
